43476416e0667e971a490bceaf43d2ac404a2a33
Yarins2000 <yarinsabag2@gmail.com>
2023-02-28 15:40:30 +0200

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# TalkBack.Server
-The server side of TalkBack project. Using Asp.Net core 6, EF core and Identity library
--- TalkBack.Server/Services/Token/IJWTTokenGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using TalkBack.Models;
-namespace TalkBack.Server.Services.Token
-{
-    public interface IJWTTokenGenerator
-    {
-        //string GenerateToken(User user, IList<string> roles, IList<Claim> claims);
-        string GenerateToken(User user);
-    }
-}
