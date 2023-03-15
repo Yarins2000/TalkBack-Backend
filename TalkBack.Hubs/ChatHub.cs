353ce97af70c00355646fad461368ea8befa70f9
Yarins2000 <yarinsabag2@gmail.com>
2023-03-15 20:04:01 +0200

    improving chat and adding notifications

--- TalkBack.Hubs/ChatHub.cs
+++ TalkBack.Hubs/ChatHub.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using TalkBack.Models;
 
 namespace TalkBack.Hubs
 {
@@ -9,6 +10,8 @@ namespace TalkBack.Hubs
     [Authorize(AuthenticationSchemes = "Bearer")]
     public class ChatHub : Hub
     {
+        private static readonly Dictionary<string, List<Message>> _messages = new();
+
         private string GenerateGroupName(string user1Id, string user2Id)
         {
             var ids = new string[] { user1Id, user2Id };
@@ -21,6 +24,12 @@ namespace TalkBack.Hubs
             string groupName = GenerateGroupName(senderId, recipientId);
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
             await Clients.Caller.SendAsync("groupNameReceived", groupName);
+
+            if (!_messages.ContainsKey(groupName))
+                _messages.Add(groupName, new());
+
+            if (_messages[groupName].Count > 0)
+                await Clients.Group(groupName).SendAsync("receiveAllMessages", _messages[groupName]);
         }
 
         public async Task LeaveGroup(string groupName)
@@ -30,7 +39,18 @@ namespace TalkBack.Hubs
 
         public async Task SendMessage(string senderId, string groupName, string message)
         {
+            _messages[groupName].Add(new Message(senderId, message, DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss")));
+
+            var ids = groupName.Split('_');
+            var recipientId = ids.First(id => id != senderId);
+
             await Clients.Group(groupName).SendAsync("receiveMessage", senderId, message, DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss"));
+            await Clients.User(recipientId).SendAsync("notifyUser");
+        }
+
+        public void DeleteMessagesForGroup(string groupName)
+        {
+            _messages.Remove(groupName);
         }
     }
 }
---
+++ TalkBack.Models/Message.cs
@@ -0,0 +1,4 @@
+﻿namespace TalkBack.Models
+{
+    public record Message(string SenderId, string MessageContent, string TimeSent);
+}
